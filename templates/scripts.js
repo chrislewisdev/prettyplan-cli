c5cba5aefa3a74b065742ac1bd1b62e878df6197
Chris Lewis <cl_myles@hotmail.com>
2019-03-07 09:04:02 +1100

    Started filling out the HTML template

---
+++ templates/scripts.js
@@ -0,0 +1,45 @@
+function accordion(element) {
+    const changes = element.parentElement.getElementsByClassName('changes');
+    for (var i = 0; i < changes.length; i++) {
+        toggleClass(changes[i], 'collapsed');
+    }
+}
+
+function toggleClass(element, className) {
+    if (!element.className.match(className)) {
+        element.className += ' ' + className;
+    }
+    else {
+        element.className = element.className.replace(className, '');
+    }
+}
+
+function addClass(element, className) {
+    if (!element.className.match(className)) element.className += ' ' + className;
+}
+
+function removeClass(element, className) {
+    element.className = element.className.replace(className, '');
+}
+
+function expandAll() {
+    const sections = document.querySelectorAll('.changes.collapsed');
+
+    for (var i = 0; i < sections.length; i++) {
+        toggleClass(sections[i], 'collapsed');
+    }
+
+    toggleClass(document.querySelector('.expand-all'), 'hidden');
+    toggleClass(document.querySelector('.collapse-all'), 'hidden');
+}
+
+function collapseAll() {
+    const sections = document.querySelectorAll('.changes:not(.collapsed)');
+
+    for (var i = 0; i < sections.length; i++) {
+        toggleClass(sections[i], 'collapsed');
+    }
+
+    toggleClass(document.querySelector('.expand-all'), 'hidden');
+    toggleClass(document.querySelector('.collapse-all'), 'hidden');
+}
