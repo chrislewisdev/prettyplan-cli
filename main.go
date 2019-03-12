5c2c4255c7a54a86280e1987d3bf36f0355aad6c
Chris Lewis <cl_myles@hotmail.com>
2019-03-12 21:40:15 +1100

    Improved raw output toggle styling, moved render functions

--- main.go
+++ main.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
 	"html/template"
 	"io/ioutil"
 	"os"
-	"regexp"
-	"strconv"
-	"strings"
 
 	"github.com/chrislewisdev/prettyplan-cli/parse"
+	"github.com/chrislewisdev/prettyplan-cli/render"
 )
 
 type report struct {
@@ -110,7 +105,7 @@ To perform exactly these actions, run the following command to apply:
 	panicIfError(err)
 
 	templateFunctions := template.FuncMap{
-		"prettify": prettify,
+		"prettify": render.Prettify,
 	}
 
 	reportTemplate, err := template.New("prettyplan.html").Funcs(templateFunctions).ParseFiles("templates/prettyplan.html")
@@ -129,36 +124,4 @@ To perform exactly these actions, run the following command to apply:
 	panicIfError(err)
 
 	outputFile.Close()
-}
-
-func prettify(value string) template.HTML {
-	value = unescapeCharacters(value)
-
-	if value == "<computed>" {
-		return template.HTML("<em>&lt;computed&gt;</em>")
-	} else if strings.HasPrefix(value, "${") && strings.HasSuffix(value, "}") {
-		return template.HTML("<em>" + value + "</em")
-	} else if _, notNumber := strconv.ParseFloat(value, 32); json.Valid([]byte(value)) && notNumber != nil {
-		//\${[^}]*(").*(").*}
-		var prettyJson bytes.Buffer
-		json.Indent(&prettyJson, []byte(value), "", "  ")
-		return template.HTML(fmt.Sprintf("<pre>%v</pre>", prettyJson.String()))
-	} else {
-		return template.HTML(value)
-	}
-}
-
-func unescapeCharacters(value string) string {
-	value = strings.Replace(value, `\n`, "\n", -1)
-	value = strings.Replace(value, `\"`, "\"", -1)
-
-	//This feels dumb - but, the above unescaping might unescape some \" sequences that need to be left alone
-	//e.g. terraform properties inside json strings like ${terraform_property["index"]}
-	//So this regex will let us re-escape any quotes inside a terraform property
-	r := regexp.MustCompile(`(\${[^}]*?[^\\}])"([^}]*?})`)
-	for r.MatchString(value) {
-		value = r.ReplaceAllString(value, `$1\"$2`)
-	}
-
-	return value
 }
